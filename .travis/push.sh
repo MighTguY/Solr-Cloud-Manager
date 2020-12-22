21231b9c2bd3861704f442c636c71991eb0f789d
Lucky Sharma <lucky.sharma@walmartlabs.com>
2020-12-22 22:53:27 +0530

    updating the commit

--- .travis/push.sh
+++ .travis/push.sh
@@ -5,4 +5,4 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git config --global push.default current
 git checkout ${TRAVIS_BRANCH}
-git push https://${GH_TOKEN}@github.com/mightguy/Solr-Cloud-Manager.git
+git push https://${GITHUB_TOKEN}@github.com/mightguy/Solr-Cloud-Manager.git
